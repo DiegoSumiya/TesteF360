08a568c89b3995b579824732b0d98bfcdd47cd7c
DiegoSumiya <diego.sumiya@gmail.com>
2022-05-11 22:41:34 -0300

    Teste Klingon

---
+++ Teste_F360/Klingon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_F360.NovaPasta
+{
+    public class Klingon
+    {
+        public char[] foo = { 's', 'l', 'f', 'w', 'k' };
+        public char[] bar = { 'b', 'c', 'd', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'v', 'x', 'y', 'z' };
+        public char[] alfabetoKlingon = { 'k', 'b', 'w', 'r', 'q', 'd', 'n', 'f', 'x', 'j', 'm', 'l', 'v', 'h', 't', 'c', 'g', 'z', 'p', 's' };
+
+        public int GetPreposicoes(string texto)
+        {
+            int qtd = 0;
+            foreach (var palavra in texto.Split(" "))
+            {
+                var ultima = palavra[palavra.Length - 1];
+                if (palavra.Length == 3 && !foo.Contains(ultima) && !palavra.Contains("d"))
+                {
+                    qtd++;
+                }
+            }
+            return qtd;
+        }
+
+        public int GetVerbos(string texto)
+        {
+            int verbo = 0;
+            int primeiraPessoa = 0;
+
+            foreach (var palavra in texto.Split(" "))
+            {
+                var primeira = palavra.FirstOrDefault();
+                var ultima = palavra[palavra.Length - 1];
+
+                if (palavra.Length >= 8 && foo.Contains(ultima))
+                {
+                    verbo++;
+
+                    if (bar.Contains(primeira))
+                    {
+                        primeiraPessoa++;
+                    }
+                }
+            }
+
+            return verbo;
+        }
+
+        public int GetNumerosBonitos(string texto)
+        {
+            int qtdeNumerosBonitos = 0;
+            foreach (var palavra in texto.Split(" "))
+            {
+                long numero = 0;
+                long peso = 1;
+                foreach (var letra in palavra)
+                {
+                    numero += Array.IndexOf(alfabetoKlingon, letra) * peso;
+                    peso *= 20;
+                }
+                if (numero >= 440566 && numero % 3 == 0)
+                {
+                    qtdeNumerosBonitos++;
+                }
+            }
+            return qtdeNumerosBonitos;
+        }
+    }
+}
---
+++ Teste_F360/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Teste_F360.NovaPasta;
+
+namespace Teste_F360
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Klingon klingon = new Klingon();
+            
+            Console.WriteLine("Digite o texto: ");
+            string texto = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("A quantidade de preposições no texto são de: " + klingon.GetPreposicoes(texto));
+            Console.WriteLine();
+            Console.WriteLine("A quantidade de verbos no texto são de: " + klingon.GetVerbos(texto));
+            Console.WriteLine();
+            Console.WriteLine("A quantidade de números bonitos são de: " + klingon.GetNumerosBonitos(texto));
+        }
+    }
+}
